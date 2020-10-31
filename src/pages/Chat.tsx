68ab1e17069bdf1d1a6f91355b55c4fcc5eb8bc3
Guilherme Frota <guiquintelas96@gmail.com>
2020-10-31 12:37:36 -0300

    add reaction to chatUserEntered & chatUserLeaved

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -3,20 +3,44 @@ import { useHistory } from 'react-router-dom';
 import Layout from '../components/Layout';
 import { useSnackBarContext } from '../contexts/SnackBarContext';
 import { useUserContext } from '../contexts/UserContext';
+import { useChatUserEnteredSubscription, useChatUserLeavedSubscription } from '../graphql/generated/graphql';
 
 const Chat: React.FC = () => {
   const { user } = useUserContext();
   const history = useHistory();
   const { snackBar } = useSnackBarContext();
+  const { data: userEntered } = useChatUserEnteredSubscription();
+  const { data: userLeaved } = useChatUserLeavedSubscription();
 
   useEffect(() => {
-    if (!user) {
-      history.push('/login');
-      snackBar('Please choose your nickname before entering the chat!', 'danger');
+    async function init() {
+      if (!user) {
+        history.push('/login');
+        snackBar('Please choose your nickname before entering the chat!', 'danger');
+      }
     }
-  }, []);
+    init();
 
-  return <Layout>chat</Layout>;
+    return () => {};
+  });
+
+  useEffect(() => {
+    if (!userEntered?.chatUserEntered) {
+      return;
+    }
+
+    snackBar(`The user ${userEntered.chatUserEntered} has joined!`);
+  }, [userEntered]);
+
+  useEffect(() => {
+    if (!userLeaved?.chatUserLeaved) {
+      return;
+    }
+
+    snackBar(`The user ${userLeaved.chatUserLeaved} has leaved the chat!`);
+  }, [userLeaved]);
+
+  return <Layout>Chat placeholder</Layout>;
 };
 
 export default Chat;
